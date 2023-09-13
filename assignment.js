71e48ac6129172ad8baf8bf1445573513f0bf34c
Elmer Balbin <contact@elmer.dev>
2023-09-12 20:54:15 -0700

    Converted flowchart to code

---
+++ assignment.js
@@ -0,0 +1,11 @@
+/*
+Exercise 1
+
+Input length in feet (LFT)
+Calculate the length in cm (LCM) by multiplying LFT by 30
+Print length in cm (LCM)
+*/
+
+let ft = 10
+let cm = ft * 30
+console.log(`${ft} feet is ${cm} centimeters.`)
